1ce14a592acc6cf1633de71f9c815471c78f49d5
Adoonrat Khuditthaloet <adoonrat.k@gmail.com>
2024-05-01 15:38:01 +0700

    Add TODO

--- .vscode/logonCommand.ps1
+++ .vscode/logonCommand.ps1
@@ -1,5 +1,6 @@
 Write-host "Testing has started..." -ForegroundColor Cyan
-Start-Process -FilePath "C:\Users\WDAGUtilityAccount\Desktop\Deploy-Application.exe" -Wait
+#TODO Modify path to execute the installer
+Start-Process -FilePath "C:\Users\WDAGUtilityAccount\Desktop\7Zip\Deploy-Application.exe" -Wait
 Write-host "Installation completed" -ForegroundColor DarkGreen
 Write-host "you have 60 seconds to verify the installation before it is automatically uninstalled" -ForegroundColor Cyan
 
@@ -10,8 +11,9 @@ while (($TimeRemaining = ($EndTime - [datetime]::UtcNow)) -gt 0) {
   Write-Progress -Activity 'Waiting for...' -Status testing -SecondsRemaining $TimeRemaining.TotalSeconds
   Start-Sleep 1
 }
+#TODO Modify path to execute the installer
 
-Start-Process -FilePath "C:\Users\WDAGUtilityAccount\Desktop\Deploy-Application.exe" -ArgumentList "Uninstall" -Wait
+Start-Process -FilePath "C:\Users\WDAGUtilityAccount\Desktop\7Zip\Deploy-Application.exe" -ArgumentList "Uninstall" -Wait
 Write-host "test completed" -ForegroundColor DarkGreen
 Write-host "You can close sandbox now!" -ForegroundColor Cyan
 Read-Host -Prompt "Press any key to continue..."
